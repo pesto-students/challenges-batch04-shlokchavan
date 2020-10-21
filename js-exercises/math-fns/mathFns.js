7b7ed89441d9a788af3a93b2a3c6c64643fc7dac
Shlok Chavan <shlok@pentaknot.com>
2020-10-22 01:34:54 +0530

    Solved Math Fns

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,7 @@
+// You can use the function from the `Math` module.
+const sqrt = (baseNumber) => Math.sqrt(baseNumber);
+const power = (baseNumber, exponent) => baseNumber ** exponent;
+const round = (number) => Math.round(number);
+
+// Don't change the exported names.
+export { sqrt, power, round };
